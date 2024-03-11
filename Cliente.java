1828b3051f1e7febf9a581f1d93eb252e4fa48f2
Theo Martins <theo.martins@germinare.org.br>
2024-03-11 13:14:05 -0300

    Arrumando os bagui - Theo

--- Cliente.java
+++ Cliente.java
@@ -1,12 +1,6 @@
 public class Cliente {
-<<<<<<< HEAD
     private String nome;
     private String cpf;
     private int agencia;
     private double saldo;
-=======
-    private String name;
-    private String cpf;
->>>>>>> ac071973f57ef76028c75e8613e73bc87c1398ba
 }
-git checkout m
